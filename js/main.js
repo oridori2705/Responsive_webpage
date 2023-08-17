3820e7dd091fae1328af3b1b9eca9d988538080f
신준혁 <oridori2705@naver.com>
2023-08-17 15:23:36 +0900

    슬라이드 버튼 기능 추가
    1. 모든 슬라이드 버튼의 태그들을 가져옴 (slidePrevlist)
    2. 그 태그들의 배열을 순회하면서 기능들을 넣어줌
    3. 만약 각각의 모든 카드데이터 li 요소의 길이가 ul 요소의 clientwidth보다 작다면 버튼을 삭제해주고, 크다면 버튼을 보여줌
    (classList.clientWidth < liList.length * 260)
    4. click이벤트를 prev버튼에 넣어줌
    5. transformPrev함수선언
    6. transformPrev함수에서는 현재 클릭한 prev버튼을 인자로 받음
    7. 그 인자를 기준으로 next버튼, ul태그, data-position값, li리스트를 가져옴
    8. if (classList.clientWidth < liList.length * 260 + Number(activeLi))
    는 오른쪽으로 나열될 카드들이 넘친 상태이므로, 왼쪽으로 이동이 가능함
    9.이때 만약 if (classList.clientWidth > liList.length * 260 + Number(activeLi)) 이라면
     넘치는 li 가 없다는 뜻으로, prev 버튼은 활성화되면 안됨

--- js/main.js
+++ js/main.js
@@ -26,3 +26,81 @@ function moveBackToTop() {
 
 window.addEventListener("scroll", checkScroll);
 backToTop.addEventListener("click", moveBackToTop);
+
+/*----------------------------------------------------------------------*/
+function transformPrev(event) {
+  //event.target : 현재 클릭한 왼쪽 슬라이드버튼요소
+  const slidePrev = event.target;
+  //slideNext : 오른쪽 슬라이드 버튼요소
+  const slideNext = slidePrev.nextElementSibling;
+
+  // ul 태그 선택 : 카드리스트 배열의 부모요소
+
+  const classList = slidePrev.parentElement.parentElement.nextElementSibling;
+  // data-position값을 가져옴
+  let activeLi = classList.getAttribute("data-position");
+  // liList : 카드리스트의 데이터 배열
+  const liList = classList.getElementsByTagName("li");
+
+  /* classList.clientWidth 는 ul 태그의 실질적인 너비
+   * liList.length * 260 에서 260 은 각 li 요소의 실질 너비(margin 포함)
+   * activeLi 는 data-position 에 있는 현재 위치
+   * 즉, liList.length * 260 + Number(activeLi) 는 현재 위치부터 오른쪽으로 나열되야 하는 나머지 카드들의 너비
+   */
+
+  /* classList.clientWidth < (liList.length * 260 + Number(activeLi)) 의미는
+   * 오른쪽으로 나열될 카드들이 넘친 상태이므로, 왼쪽으로 이동이 가능함
+   */
+
+  if (classList.clientWidth < liList.length * 260 + Number(activeLi)) {
+    // 위치를 왼쪽으로 260 이동 (-260px) -> 슬라이드 오른쪽버튼 활성화
+    activeLi = Number(activeLi) - 260;
+
+    /* 위치를 왼쪽으로 260 이동 (-260px)
+     * 해당 위치는 변경된 activeLi 값이 적용된 liList.length * 260 + Number(activeLi) 값임
+     * 이 값보다, classList.clientWidth (ul 태그의 너비)가 크다는 것은
+     * 넘치는 li 가 없다는 뜻으로, prev 버튼은 활성화되면 안됨
+     */
+    if (classList.clientWidth > liList.length * 260 + Number(activeLi)) {
+      //더이상 왼쪽으로 갈 수 가 없음 -> 왼쪽 버튼 비활성화
+      slidePrev.style.color = "#cfd8dc";
+      slidePrev.classList.remove("slide-prev-hover");
+    }
+    //오른쪽버튼을 활성화 -> 왼쪽버튼을 눌러서 슬라이드가 왼쪽으로 밀렸다면 무조건 오른쪽버튼은 활성화되야된다.
+    slideNext.style.color = "#2f3059";
+    slideNext.classList.add("slide-next-hover");
+  }
+
+  //ul자체를 activeLi값만큼 이동함 ->data-position
+  classList.style.transition = "transform 1s";
+  classList.style.transform = "translateX(" + String(activeLi) + "px)";
+  classList.setAttribute("data-position", activeLi);
+}
+
+//모든 슬라이드버튼의 요소들을 배열로 가져옴
+const slidePrevList = document.getElementsByClassName("slide-prev");
+
+//모든 카드리스트의 슬라이드 버튼을 순회하는 것임
+for (let i = 0; i < slidePrevList.length; i++) {
+  // ul 태그 선택, prev버튼의 부모의 부모의 바로 다음 형제 요소를 선택한 것임
+  let classList =
+    slidePrevList[i].parentElement.parentElement.nextElementSibling;
+  /*카드들의 데이터가 배열로 생성됨 */
+  let liList = classList.getElementsByTagName("li");
+
+  // 카드가 ul 태그 너비보다 넘치면, 왼쪽(PREV) 버튼은 활성화하고, 오른쪽(NEXT)는 현재 맨 첫카드 위치이므로 비활성화
+  // clientWidth : 요소의 너비
+  // 260 : 카드 요소 하나의 너비와 마진(좌,우)을 합한 값
+  if (classList.clientWidth < liList.length * 260) {
+    slidePrevList[i].classList.add("slide-prev-hover");
+    slidePrevList[i].addEventListener("click", transformPrev);
+  } else {
+    /* 태그 삭제시, 부모 요소에서 removeChild 를 통해 삭제해야 함
+           따라서, 1. 먼저 부모 요소를 찾아서,
+                 2. 부모 요소의 자식 요소로 있는 PREV 와 NEXT 요소를 삭제함
+        */
+    const arrowContainer = slidePrevList[i].parentElement;
+    arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+    arrowContainer.removeChild(slidePrevList[i]);
+  }
+}
